ceabb12beceeaa3029c30c3faab853a3de947e52
mickypardo <mapardobornes@gmail.com>
2023-03-16 23:07:42 +0100

    Implementation of Spring Initializr

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# HOTELS'R'US
+A hotel booking api rest, which allows users to book rooms in different hotels based on the availability of hotel dates.
---
+++ src/main/java/org/mpardo/hotelsrus/HotelsRUsApplication.java
@@ -0,0 +1,13 @@
+package org.mpardo.hotelsrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsRUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsRUsApplication.class, args);
+	}
+
+}
