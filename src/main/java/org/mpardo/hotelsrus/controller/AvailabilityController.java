ff5e1b411e3c5e35e1e829fb08a8ceaf1c13b245
MickyPardo <mapardobornes@gmail.com>
2023-03-18 15:33:53 +0100

    Implementing controller layer - BookingController Class

--- src/main/java/org/mpardo/hotelsrus/controller/AvailabilityController.java
+++ src/main/java/org/mpardo/hotelsrus/controller/AvailabilityController.java
@@ -1,5 +1,10 @@
 package org.mpardo.hotelsrus.controller;
 
+import org.mpardo.hotelsrus.service.IAvailabilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Controlador de Availability que realiza de intermediario entre el Frontend y el Servicio.
  * 
@@ -8,6 +13,17 @@ package org.mpardo.hotelsrus.controller;
  * @version 1.0
  *
  */
+@RestController
+@RequestMapping("/availabilities")
 public class AvailabilityController {
 
+	private IAvailabilityService availabilityService;
+	
+	@Autowired
+	public AvailabilityController(IAvailabilityService availabilityService) {
+		
+		this.availabilityService = availabilityService;
+		
+	}
+	
 }
--- src/main/java/org/mpardo/hotelsrus/controller/BookingController.java
+++ src/main/java/org/mpardo/hotelsrus/controller/BookingController.java
@@ -1,5 +1,20 @@
 package org.mpardo.hotelsrus.controller;
 
+import java.time.LocalDate;
+
+import org.mpardo.hotelsrus.dto.BookingDTO;
+import org.mpardo.hotelsrus.dto.HotelDTO;
+import org.mpardo.hotelsrus.model.Booking;
+import org.mpardo.hotelsrus.model.Hotel;
+import org.mpardo.hotelsrus.service.IBookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Controlador de Booking que realiza de intermediario entre el Frontend y el Servicio.
  * 
@@ -8,6 +23,41 @@ package org.mpardo.hotelsrus.controller;
  * @version 1.0
  *
  */
+@RestController
+@RequestMapping("/bookings")
 public class BookingController {
 
+	private IBookingService bookingService;
+	
+	@Autowired
+	public BookingController(IBookingService bookingService) {
+	
+		this.bookingService = bookingService;
+		
+	}
+	
+	// Hacer una reserva de un hotel en la BBDD
+	@PostMapping("/res")
+	public ResponseEntity<?> makeBooking(@RequestBody BookingDTO bookingDTO) {
+		
+		HotelDTO hotelDTO = bookingDTO.getHotelDTO();
+		Hotel hotel = new Hotel(hotelDTO.getName(), hotelDTO.getCategory());
+		LocalDate dateFrom = bookingDTO.getDateFrom();
+		LocalDate dateTo = bookingDTO.getDateTo();
+		String email = bookingDTO.getEmail();
+		
+		Booking booking = new Booking(hotel, dateFrom, dateTo, email);
+		
+		bookingService.create(booking);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+		
+	}
+	
+	// Consultar todas las reservas de un hotel
+	
+	// Obtener una reserva específica del hotel
+	
+	// Cancelar una reserva
+	
 }
